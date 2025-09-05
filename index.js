9cc07d43bc0ad2441c81ed47559509ab7b2a8b40
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-09-05 12:11:09 +0100

    Render menu items

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 import { menuArray } from "./data.js";
+
+const menu = document.getElementById("menu");
+
+const mappedMenu = menuArray
+  .map((item) => {
+    return `
+    <div class="menu-item">
+      <div class="item-left-container">
+        <span class="item-emoji">${item.emoji}</span>
+        <div >
+          <h3 class="item-name">${item.name}</h3>
+          <p class="item-ingredients">${item.ingredients.join(", ")}</p>
+          <p class="item-price">£${item.price}</p>
+        </div>
+      </div>
+
+      <button data-menu-item-id="${
+        item.id
+      }" id="addItemBtn" class="item-btn">+</button>
+    </div>
+    <hr />
+  `;
+  })
+  .join("");
+
+menu.innerHTML = mappedMenu;
