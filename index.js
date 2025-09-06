0b484b20d52b4033fcaac6f66dab83d9df8ec5da
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-09-06 14:41:16 +0100

    Style confirmation message

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ document.getElementById("payment-form").addEventListener("submit", (e) => {
 
   // Render confirmation message
   let customerName = "James"; // Temporary
-  bottomSection.innerHTML = `<div class="confirmation-message-container"><p class="confirmation-message">Thanks, ${customerName}. Your order is on its way!</p></div>`;
+  bottomSection.innerHTML = `<div class="confirmation-message"><p>Thanks, ${customerName}.<br />Your order is on its way!</p></div>`;
 });
 
 // Functions for adding/removing
